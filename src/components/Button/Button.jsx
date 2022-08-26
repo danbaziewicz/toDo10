f3408edecb0412aea539d6f38c22ccdac82ea438
danbaziewicz <danxbazi@gmail.com>
2022-08-26 19:41:27 -0300

    feat: criação de componente button base

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({onClick, style, txtBtn}) => {
+  return (
+    <button onClick={onClick} className={style}>{txtBtn}</button>
+  )
+}
+
+export default Button
