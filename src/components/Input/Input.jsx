2407eaad3ee11fa8675e0c1a5a1c6c7c074d0da7
danbaziewicz <danxbazi@gmail.com>
2022-08-28 23:02:45 -0300

    ajuste de props no componente Input.

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Input = ({type, style, name}) => {
+const Input = ({type, style, name, value, onChange}) => {
   return (
-    <input className={style} type={type} name={name}/>
+    <input className={style} type={type} name={name} value={value} onChange={onChange}/>
   )
 }
 
