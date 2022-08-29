475324cc9988319287367364791fe195b883b2f1
danbaziewicz <danxbazi@gmail.com>
2022-08-29 15:29:59 -0300

    fix: ajuste de obj dadosForm setado dentro de listaCli.

--- src/context/ClientesProvider.jsx
+++ src/context/ClientesProvider.jsx
@@ -1,90 +1,83 @@
-import React, { createContext, useState} from "react";
+import React, { createContext, useState } from "react";
 
-export const ClientesContext = createContext()
+export const ClientesContext = createContext();
 
-const ClientesProvider = ({children}) => {
+const ClientesProvider = ({ children }) => {
+  const [dadosForm, setDadosForm] = useState({
+    nome: "",
+    sobrenome: "",
+    email: "",
+    telefone: "",
+    cep: "",
+    enderecoUm: "",
+    enderecoDois: "",
+    nascimento: "",
+    cpf: "",
+    renda: "",
+  });
 
-    const [dadosForm, setDadosForm] = useState({
-        nome: '',
-        sobrenome: '',
-        email: '',
-        telefone: '',
-        cep: '',
-        enderecoUm: '',
-        enderecoDois: '',
-        nascimento: '',
-        cpf: '',
-        renda: ''
-    })
-    
-    const [dados, setDados] = useState({})
-    const [listaCli, setListaCli] = useState([])
+  const [listaCli, setListaCli] = useState([]);
 
-    function handleChange (e, chave) {
-        setDadosForm({...dadosForm, [chave]:e.target.value})
+  function handleChange(e, chave) {
+    setDadosForm({ ...dadosForm, [chave]: e.target.value });
+  }
+
+  const [view, setView] = useState("form1");
+
+  function handleNextForm() {
+    if (view == "form1") {
+      setView("form2");
     }
 
-    const [view, setView] = useState('form1')
-
-    function handleNextForm() {
-        console.log('entrou')
-        if(view == 'form1'){
-          setView('form2')
-        }
-    
-        if(view == 'form2') {
-          setView('form3')
-        }
-      }
-        
-    function handleClick(e) {
-        e.preventDefault()
-        setDados({...dados, dadosForm})
-        // console.log(dados)
-        if(view == 'form1'){
-            handleNextForm()
-        } else if(view == 'form2') {
-            handleNextForm()
-        } else if(view == 'form3') {
-            handleFinaliza()
-            setDadosForm({
-                nome: '',
-                sobrenome: '',
-                email: '',
-                telefone: '',
-                cep: '',
-                enderecoUm: '',
-                enderecoDois: '',
-                nascimento: '',
-                cpf: '',
-                renda: ''
-            })
-            setView('form1')
-        } 
-        // console.log(dados)
+    if (view == "form2") {
+      setView("form3");
     }
+  }
 
-    function handleFinaliza() {
-        setDados({...dados, dadosForm})
-        setListaCli([...listaCli, dados])
-        console.log(dados)
-        console.log(dadosForm)
+  function handleClick(e) {
+    e.preventDefault();
+    if (view == "form1") {
+      handleNextForm();
+    } else if (view == "form2") {
+      handleNextForm();
+    } else if (view == "form3") {
+      handleFinaliza();
+      setDadosForm({
+        nome: "",
+        sobrenome: "",
+        email: "",
+        telefone: "",
+        cep: "",
+        enderecoUm: "",
+        enderecoDois: "",
+        nascimento: "",
+        cpf: "",
+        renda: "",
+      });
+      alert(
+        `Cliente ${dadosForm.nome} ${dadosForm.sobrenome} cadastrado com sucessso`
+      );
+      setView("form1");
     }
+  }
 
-    const contexto = {
-        dadosForm: dadosForm,
-        listaCli: listaCli,
-        handleChange: handleChange,
-        handleClick: handleClick,
-        view: view
-    }
+  function handleFinaliza() {
+    setListaCli([...listaCli, dadosForm]);
+  }
 
-    return(
-        <ClientesContext.Provider value={contexto}>
-            {children}
-        </ClientesContext.Provider>
-    )
+  const contexto = {
+    dadosForm: dadosForm,
+    listaCli: listaCli,
+    handleChange: handleChange,
+    handleClick: handleClick,
+    view: view,
+  };
 
-}
+  return (
+    <ClientesContext.Provider value={contexto}>
+      {children}
+    </ClientesContext.Provider>
+  );
+};
 
-export default ClientesProvider
+export default ClientesProvider;
