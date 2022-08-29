f9345ba2a7450143ddd2d63cf70531b71911854e
danbaziewicz <danxbazi@gmail.com>
2022-08-28 22:58:37 -0300

    feat: criada rota home

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import MenuLateral from './components/MenuLateral/MenuLateral'
 import Cadastro from './pages/cadastro/Cadastro'
+import Home from './pages/Home/Home'
 
 function App() {
 
@@ -10,10 +11,12 @@ function App() {
     <div>
     <BrowserRouter>
     <MenuLateral/>
+    {/* HEADER */}
       <Routes>
         <Route path='/cadastro' element={<Cadastro/>} />
-
+        <Route path='/' element={<Home/>} />
       </Routes>
+    {/* FOOTER */}
     </BrowserRouter>
     </div>
   )
